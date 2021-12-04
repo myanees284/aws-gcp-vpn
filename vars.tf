c4475825e42f144c0ff4ae535c0916873a108729
Anees Mohammed Amzad Mahomood <44027805+myanees284@users.noreply.github.com>
2021-12-04 22:09:54 +0400

    Update vars.tf
    updated the BGP numbers to default values

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "AWS_REGION" { default = "t" }
 variable "AWS_VPC_ID" { default = "t" }
-variable "AWS_BGP" { default = "t" }
-variable "GCP_BGP" { default = "t" }
+variable "AWS_BGP" { default = "64512" }
+variable "GCP_BGP" { default = "65420" }
 
 variable "GCP_PROJECT" { default = "t" }
 variable "GCP_REGION" { default = "t" }
